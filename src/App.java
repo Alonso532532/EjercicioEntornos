bfedeed312d8258ebecc99c29ab82d8c17053976
Alonso <asorianom2007@gmail.com>
2025-11-21 20:47:43 +0100

    Primer commit

---
+++ src/App.java
@@ -0,0 +1,34 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println(String.format("Práctica 1 Alonso"));
+
+        Scanner teclado = new Scanner(System.in);
+
+        for (; true; ) {
+            System.out.println("Choose an option:\n" +
+                    "  1. Square\n" +
+                    "  2. Title\n" +
+                    "  0. Quit\n");
+
+            int numero = teclado.nextInt();
+
+            if (numero == 1) {
+                System.out.printf("Lado del cuadrado: ");
+                int lado = teclado.nextInt();
+                Figures.writeSquare(lado);
+            } else if (numero == 2) {
+                System.out.printf("Titulo: ");
+                String titulo = teclado.nextLine();
+                titulo = teclado.nextLine();
+                Titles.writeTitle(titulo);
+            } else if (numero == 0) {
+                Console.close();
+            }
+        }
+    }
+}
