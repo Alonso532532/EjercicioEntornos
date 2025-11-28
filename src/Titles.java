61c9dc7fbf9977846066dc16e021707909d22680
Alonso <alonsoriano2007@gmail.com>
2025-11-28 23:23:40 +0100

    Modificación de la clase titles

--- src/Titles.java
+++ src/Titles.java
@@ -2,18 +2,11 @@ public class Titles {
 
     public static void writeTitle(String c) {
 
-        System.out.println("");
+        for (int i = 0; i < c.length() + 8; i++) System.out.print("*");
+        System.out.println();
+        System.out.println("*** " + c + " ***");
+        for (int i = 0; i < c.length() + 8; i++) System.out.print("*");
+        System.out.println();
 
-        for(int i=0;i<80;i++){
-            System.out.printf("*");
-        }
-        System.out.println("");
-
-        System.out.println(c);
-
-        for(int i=0;i<80;i++){
-            System.out.printf("*");
-        }
-        System.out.println("");
     }
 }
